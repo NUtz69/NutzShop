57f20a706d6c028239849c5c47740766156c8949
Nutz <Nutz@DESKTOP-2AHDRPS>
2021-01-05 15:15:29 +0100

    Product Manager Pages Built & Test

--- NutzShop.WebUI/Controllers/ProductManagerController.cs
+++ NutzShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NutzShop.Core.Models;
-using NutzShop.DataAccess.InMemory;
+using NutzShop.DataAccess.InMerory;
 
 namespace NutzShop.WebUI.Controllers
 {
