23d44adcbbf89cefbad5986bccf13439da0d38a3
Nutz <Nutz@DESKTOP-2AHDRPS>
2021-01-19 15:50:23 +0100

    Added Basket Model

---
+++ NutzShop.Core/Models/BasketItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutzShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        // Var
+        public string BasketId { get; set; }
+        public string ProductId { get; set; }
+        public int Quanity { get; set; }
+    }
+}
