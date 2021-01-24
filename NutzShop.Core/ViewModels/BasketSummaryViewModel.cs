6a66bf029c67cd435a5e81f8138cc8999cf4a3f2
Nutz <Nutz@DESKTOP-2AHDRPS>
2021-01-24 16:00:14 +0100

    Finished Basket Service

---
+++ NutzShop.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutzShop.Core.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        // Vars
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        // Constructors
+        public BasketSummaryViewModel()
+        {
+
+        }
+
+        // Constructors - Default
+        public BasketSummaryViewModel(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+    }
+}
